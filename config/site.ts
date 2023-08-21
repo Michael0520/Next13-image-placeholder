bf2a9fb0e4731d783020adfbbc08ba3ea0b7b11a
Michael0520 <po2319987@gmail.com>
2023-08-22 03:39:36 +0800

    feat: 🎸 update config

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Next.js Image PlaceHolder",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
@@ -11,8 +11,12 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    github: "https://github.com/Michael0520/Next13-image-placeholder",
   },
+  routes: [
+    {
+      title: "Base64 Blur",
+      href: '/base64'
+    }
+  ]
 }
